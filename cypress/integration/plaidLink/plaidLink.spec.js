990bcac964dddd5ea17d0fa097a63631b5150c38
Dan Beaumont <danielbeaumont95@hotmail.co.uk>
2022-06-29 20:51:06 +0100

    cypress tests now finds plaid link pop up

--- cypress/integration/plaidLink/plaidLink.spec.js
+++ cypress/integration/plaidLink/plaidLink.spec.js
@@ -1,12 +1,25 @@
 /// <reference types="Cypress" />
 
 describe('tests plaid link page', () => {
-  it('goes through plaid link successfully', () => {
+  it('goes through plaid link', () => {
     cy.loginToApp('firstuser@hotmail.com', 'password123');
     cy.visit('http://localhost:3000/plaidLink');
     cy.get('.connectBankAccountContainer').should('exist');
     cy.get('.linkPlaidAccount').should('exist').click();
-    cy.wait(4000);
-    cy.get('.iframes-container').should('exist');
+    cy.get('iframe#plaid-link-iframe-1', { timeout: 30000 }).then((iframe) => {
+      cy.wrap(iframe) // from Cypress blog ref above
+        .its('0.contentDocument.body')
+        .should('not.be.empty') // ensure the iframe body is loaded
+        .as('iframeBody') // save for further commands within iframe.body
+
+        .find('#aut-button', { timeout: 10000 })
+        .click();
+
+      cy.get('@iframeBody')
+        .contains('h1', 'Select your bank'); // confirm the result of the click()
+
+      cy.get('@iframeBody')
+        .contains('h2', 'HSBC (UK) - Personal');
+    });
   });
 });
