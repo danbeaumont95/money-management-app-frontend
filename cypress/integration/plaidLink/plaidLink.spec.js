3889917bd90ad61e02cdf3f3d94f4974e6f69f26
Dan Beaumont <danielbeaumont95@hotmail.co.uk>
2022-07-04 19:43:02 +0100

    added navbar to all pages, and added better styling to plaidLink screen

--- cypress/integration/plaidLink/plaidLink.spec.js
+++ cypress/integration/plaidLink/plaidLink.spec.js
@@ -5,6 +5,8 @@ describe('tests plaid link page', () => {
     cy.loginToApp('firstuser@hotmail.com', 'password123');
     cy.visit('http://localhost:3000/plaidLink');
     cy.get('.connectBankAccountContainer').should('exist');
+    cy.get('.plaidImage').should('exist');
+    cy.get('.plaidText').should('exist');
     cy.get('.linkPlaidAccount').should('exist').click();
     cy.get('iframe#plaid-link-iframe-1', { timeout: 30000 }).then((iframe) => {
       cy.wrap(iframe) // from Cypress blog ref above
